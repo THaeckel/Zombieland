3cf4167b995bfa88c7f00135bab16ff0548799f5
Timo Haeckel <TimoHaeckel@gmx.de>
2014-02-23 22:56:41 +0100

    Created Potion Class

---
+++ ZombieLand/item/itemPotion/HealthPotion.java
@@ -0,0 +1,14 @@
+package itemPotion;
+
+public class HealthPotion extends Potion {
+
+	public HealthPotion(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void useItem() {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ ZombieLand/item/itemPotion/Potion.java
@@ -0,0 +1,17 @@
+package itemPotion;
+
+import item.Item;
+import item.UsableItem;
+
+public class Potion extends Item implements UsableItem{
+
+	public Potion(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void useItem() {
+		// TODO Auto-generated method stub
+	}
+}
